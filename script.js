40175c6d7209568dee96305f6e6291d6de417bfe
A. Santiago Nunez <abraham.nunez@tecsup.edu.pe>
2025-06-09 03:10:21 -0500

    Sitio personal añadido

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Tarjeta profesional cargada.");
+    console.log("Tarjeta profesional cargada.");
 });
 
-  let pos = 0;
-  setInterval(() => {
+let pos = 0;
+setInterval(() => {
     pos++;
     document.body.style.backgroundPosition = `${pos}px 0`;
-  }, 30); // cada 30ms, ajusta según velocidad deseada
+}, 30); // cada 30ms, ajusta según velocidad deseada
